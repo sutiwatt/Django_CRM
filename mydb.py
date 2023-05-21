1ccb40f20e441fcc02c4f67a3da78e9285d3f386
sutiwatt <sutiwattboom@gmail.com>
2023-05-21 18:57:34 +0700

    Initail Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password123'
+)
+
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco_1")
+
+print("already create")
